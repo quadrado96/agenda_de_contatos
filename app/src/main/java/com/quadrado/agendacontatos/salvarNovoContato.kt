3f2ea7d11f153b67ed8a3f72ef4b31afda26ff4b
Brunno Quadrado <bbertoninuti@gmail.com>
2025-03-01 16:08:13 -0300

    floating action buttons and new activity

--- app/src/main/java/com/quadrado/agendacontatos/salvarNovoContato.kt
+++ app/src/main/java/com/quadrado/agendacontatos/salvarNovoContato.kt
@@ -1,30 +1,27 @@
 package com.quadrado.agendacontatos
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class MainActivity : AppCompatActivity() {
+class salvarNovoContato : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_salvar_novo_contato)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val listaContatos = findViewById<RecyclerView>(R.id.rv_Contatos)
-
-        val salvarContato = Intent(this, salvarNovoContato::class.java)
-        val fab_adicionarContato = findViewById<FloatingActionButton>(R.id.fab_adicionarContato).setOnClickListener() {
-            startActivity(salvarContato)
+        val salvarContato = findViewById<FloatingActionButton>(R.id.fab_salvarContato).setOnClickListener() {
+            finish()
         }
+
+
     }
 }
