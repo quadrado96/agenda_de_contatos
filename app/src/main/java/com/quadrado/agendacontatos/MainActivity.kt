3a31fbf57146b8bc3702b46544a3884fa831d4c2
Brunno Quadrado <bbertoninuti@gmail.com>
2025-03-02 14:48:38 -0300

    configured list to main activity

--- app/src/main/java/com/quadrado/agendacontatos/MainActivity.kt
+++ app/src/main/java/com/quadrado/agendacontatos/MainActivity.kt
@@ -2,14 +2,20 @@ package com.quadrado.agendacontatos
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isEmpty
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var adapter: ContatoAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,11 +28,29 @@ class MainActivity : AppCompatActivity() {
 
         supportActionBar?.setTitle(R.string.app_name)
 
-        val listaContatos = findViewById<RecyclerView>(R.id.rv_Contatos)
+        listarContatos()
 
-        val salvarContato = Intent(this, salvarNovoContato::class.java)
+        val salvarContato = Intent(this, SalvarNovoContato::class.java)
         val fab_adicionarContato = findViewById<FloatingActionButton>(R.id.fab_adicionarContato).setOnClickListener() {
             startActivity(salvarContato)
+        }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        listarContatos()
+    }
+
+    private fun listarContatos() {
+        val listaContatos = findViewById<RecyclerView>(R.id.rv_Contatos)
+        listaContatos.layoutManager = LinearLayoutManager(this)
+
+        adapter = ContatoAdapter(mutableListOf())
+        listaContatos.adapter = adapter
+
+        if (adapter.itemCount == 0) {
+            Toast.makeText(this, "Essa lista está vazia.", Toast.LENGTH_LONG).show()
         }
     }
 }
--- app/src/main/java/com/quadrado/agendacontatos/SalvarNovoContato.kt
+++ app/src/main/java/com/quadrado/agendacontatos/SalvarNovoContato.kt
@@ -7,7 +7,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class salvarNovoContato : AppCompatActivity() {
+class SalvarNovoContato : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
